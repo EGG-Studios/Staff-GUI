1943bd224a7148dbd913b282aa20286f1ac7d413
foxie <fabianverhagen2011@gmail.com>
2025-11-20 09:39:59 +0100

    Initial commit

---
+++ src/main/kotlin/org/eggstudios/staffGUI/StaffGUI.kt
@@ -0,0 +1,14 @@
+package org.eggstudios.staffGUI
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class StaffGUI : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
