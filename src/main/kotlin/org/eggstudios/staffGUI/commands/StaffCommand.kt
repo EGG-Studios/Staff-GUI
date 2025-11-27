3721f91125c28b462b087ec6be1139e00a9cb75c
foxie <fabianverhagen2011@gmail.com>
2025-11-27 09:23:06 +0100

    Added /staff command

---
+++ src/main/kotlin/org/eggstudios/staffGUI/commands/StaffCommand.kt
@@ -0,0 +1,16 @@
+package org.eggstudios.staffGUI.commands
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+class StaffCommand : CommandExecutor {
+    override fun onCommand(
+        p0: CommandSender,
+        p1: Command,
+        p2: String,
+        p3: Array<out String>
+    ): Boolean {
+        TODO("Open The dialog")
+    }
+}
